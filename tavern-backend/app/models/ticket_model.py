daca18b69ee0f970a5f723fce84aa31ea29b745b
nuttaphat <nuttaphat.aruno@gmail.com>
2022-10-27 11:03:03 +0800

    refactor(tavern-backend): fix and add docstring to Ticket model (#11)

--- tavern-backend/app/models/ticket_model.py
+++ tavern-backend/app/models/ticket_model.py
@@ -6,10 +6,18 @@ from pydantic import BaseModel, Field
 
 
 ################################################################################
-# Token Model
-# - Token
+# Ticket Model
+# - Ticket
 ################################################################################
 class Ticket(BaseModel):
+    """Model for Ticket data
+
+    Attrs:
+        _id (str): Unique id. Defaults to HelperService.generate_hex_string().
+        number (int): Unique ticket number. Defaults to HelperService.generate_ticket_number().
+        is_used (bool): Ticket usage indicator
+        created_at (float): Date created of the scroll. Defaults to time.time()
+    """
     _id: str = Field(default_factory=HelperService.generate_hex_string())
     number: str = Field(default_factory=HelperService.generate_ticket_number())
     is_used: bool = False
